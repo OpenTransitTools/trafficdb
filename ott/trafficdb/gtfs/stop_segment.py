da6dffaabea447832982c835db70c0b8cdb6c0af
Frank Purcell <fxpurcell@gmail.com>
2020-11-26 09:48:30 -0800

    fix(db): more segment db work

--- ott/trafficdb/gtfs/stop_segment.py
+++ ott/trafficdb/gtfs/stop_segment.py
@@ -23,7 +23,7 @@ class StopSegment(Base):
     """
 
     def __init__(self, begin_stop, end_stop):
-        self.segment_id = format("{}-{}", begin_stop.stop_id, end_stop.stop_id)
+        self.segment_id = self.make_id(begin_stop, end_stop)
 
     @classmethod
     def factory(cls, begin_stop, end_stop):
@@ -34,25 +34,57 @@ class StopSegment(Base):
         return ret_val
 
     @classmethod
+    def _make_id(cls, begin_stop, end_stop):
+        return "{}-{}".format(begin_stop.stop_id, end_stop.stop_id)
+
+    @classmethod
+    def _cache_segment(cls, cache, begin_stop, end_stop, trip):
+        id = cls._make_id(begin_stop, end_stop)
+        if id not in cache:
+            cache[id] = {
+                'id': id,
+                'begin_time': begin_stop.arrival_time,
+                'end_time': end_stop.departure_time,
+                'begin_stop': begin_stop,
+                'end_stop': end_stop,
+            }
+        else:
+            # check for earlier segment begin_time than what's cached
+            if begin_stop.arrival_time < cache[id]['begin_time']:
+                cache[id]['begin_time'] = begin_stop.arrival_time
+            # check for later segment end_time than what's cached
+            if end_stop.departure_time > cache[id]['end_time']:
+                cache[id]['end_time'] = end_stop.departure_time
+
+
+    @classmethod
     def load(cls, session):
         """
         will find ...
         """
+        cache = {}
         try:
+            # step 1: query gtfsdb and build a cache of stop-stop segments
             from gtfsdb import Trip
-            t = session.query(Trip).limit(1).one()
-            stop_times = t.stop_times
-            stop_times_len = len(stop_times)
-            if stop_times_len > 1:
-                for i, st in enumerate(stop_times):
-                    begin_stop = stop_times[i]
-                    end_stop = stop_times[i+1]
-                    print(begin_stop.stop_sequence, end_stop.stop_sequence)
-                    if i+2 >= stop_times_len:
-                        break
+            trips = session.query(Trip).filter(Trip.route_id == '70')
+            for t in trips.all():
+                stop_times = t.stop_times
+                stop_times_len = len(stop_times)
+                if stop_times_len > 1:
+                    for i, st in enumerate(stop_times):
+                        begin_stop = stop_times[i]
+                        end_stop = stop_times[i+1]
+                        cls._cache_segment(cache, begin_stop, end_stop, t)
+                        if i+2 >= stop_times_len:
+                            break
 
+            # step 2: write this cache to db
+            print(len(cache))
+            for c in cache:
+                print(cache[c])
         except Exception as e:
             log.exception(e)
+
 
     @classmethod
     def clear_tables(cls, session, agency):
