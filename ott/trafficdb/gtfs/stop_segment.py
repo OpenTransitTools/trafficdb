a94b05e77bec8a771f2d326b83381f22c34ad711
Frank Purcell <fxpurcell@gmail.com>
2020-11-26 21:03:25 -0800

    fix(db): calc distance and find shapes in the segment

--- ott/trafficdb/gtfs/stop_segment.py
+++ ott/trafficdb/gtfs/stop_segment.py
@@ -1,4 +1,8 @@
-from sqlalchemy import Column, Index, Integer, Numeric, String, DateTime
+from sqlalchemy import Column, String
+
+from gtfsdb import Trip
+from gtfsdb import Shape
+
 from ott.trafficdb.gtfs.base import Base
 
 import logging
@@ -38,15 +42,30 @@ class StopSegment(Base):
         return "{}-{}".format(begin_stop.stop_id, end_stop.stop_id)
 
     @classmethod
-    def _cache_segment(cls, cache, begin_stop, end_stop, trip):
+    def _make_shapes(cls, session, begin_stop, end_stop, trip):
+        """
+        return the shape points between two stops along a trip
+        TODO: could be a gtfsdb utility belonging to Shape. class
+        """
+        ret_val = session.query(Shape)\
+            .filter(Shape.shape_id == trip.shape_id)\
+            .filter(Shape.shape_dist_traveled >= begin_stop.shape_dist_traveled)\
+            .filter(Shape.shape_dist_traveled <= end_stop.shape_dist_traveled)\
+            .all()
+        return ret_val
+
+    @classmethod
+    def _cache_segment(cls, session, cache, begin_stop, end_stop, trip):
         id = cls._make_id(begin_stop, end_stop)
         if id not in cache:
             cache[id] = {
                 'id': id,
                 'begin_time': begin_stop.arrival_time,
                 'end_time': end_stop.departure_time,
-                'begin_stop': begin_stop,
-                'end_stop': end_stop,
+                'begin_stop': begin_stop.stop,
+                'end_stop': end_stop.stop,
+                'distance': end_stop.shape_dist_traveled - begin_stop.shape_dist_traveled,
+                'shape': cls._make_shapes(session, begin_stop, end_stop, trip)
             }
         else:
             # check for earlier segment begin_time than what's cached
@@ -65,7 +84,6 @@ class StopSegment(Base):
         cache = {}
         try:
             # step 1: query gtfsdb and build a cache of stop-stop segments
-            from gtfsdb import Trip
             trips = session.query(Trip).filter(Trip.route_id == '70')
             for t in trips.all():
                 stop_times = t.stop_times
@@ -74,7 +92,7 @@ class StopSegment(Base):
                     for i, st in enumerate(stop_times):
                         begin_stop = stop_times[i]
                         end_stop = stop_times[i+1]
-                        cls._cache_segment(cache, begin_stop, end_stop, t)
+                        cls._cache_segment(session, cache, begin_stop, end_stop, t)
                         if i+2 >= stop_times_len:
                             break
 
