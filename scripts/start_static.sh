0f45fce75bd7d3f894d9a3c8dc386a87b7aeba2b
Frank Purcell <fxpurcell@gmail.com>
2020-11-30 13:16:12 -0800

    fix(script): open map / move start script

---
+++ scripts/start_static.sh
@@ -0,0 +1,12 @@
+PORT=8000
+
+DIR=`dirname $0`
+cd $DIR/../ott/trafficdb/examples/
+
+URL=http://localhost:$PORT/map.html#10.00/45.48/-122.68/0/20
+echo $URL
+python -m webbrowser $URL
+try:
+  python -m SimpleHTTPServer $PORT
+except:
+  python -m http.server $PORT
