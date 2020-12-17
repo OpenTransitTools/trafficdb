057bee7643fe5d77a63f3ba5517bfc0cce306ee2
Frank Purcell <fxpurcell@gmail.com>
2020-12-17 00:09:40 -0800

    fix(speed): more INRIX token / query managment work

--- ott/trafficdb/control/inrix/base.py
+++ ott/trafficdb/control/inrix/base.py
@@ -16,20 +16,35 @@ def make_inrix_url(service, token=None, force=False, format="json"):
     return url
 
 
-def speeds_url_state(geo_id="243"):
+def speeds_url_state(geo_id="243", force=False):
     """
     Oregon is geoID=243
     http://api.inrix.com/Traffic/Inrix.ashx?action=GetSegmentSpeedInGeography&geoID=243&token=
     :see: http://docs.inrix.com/traffic/speed/
     """
-    url = "{}&geoID={}".format(make_inrix_url("GetSegmentSpeedInGeography"), geo_id)
+    url = "{}&geoID={}".format(make_inrix_url("GetSegmentSpeedInGeography", force=force), geo_id)
     return url
 
 
-def speeds_url_bbox(bbox=None):
+def speeds_url_bbox(bbox=None, force=False):
     """
     ?Action=GetSegmentSpeedinBox
     :see: http://docs.inrix.com/traffic/speed/
     """
-    url = "{}&bbox={}".format(make_inrix_url("GetSegmentSpeedinBox"), bbox)
+    url = "{}&bbox={}".format(make_inrix_url("GetSegmentSpeedinBox", force=force), bbox)
+    return url
+
+
+def speeds_url_segments(segment_ids="385703529,385862590,385869437,385869437,440964536,440964537", force=False):
+    """
+    13th & Bybee Blvd are 385703529,385862590,385869437,385869437,440964536,440964537
+
+    http://{serverPath}.INRIX.com/traffic/Inrix.ashx?Action=GetSegmentSpeed&token={tokenHere}
+    &Segments=5908854|XDS,5898079|XDS,5898012|XDS,111%2B11479|TMC,113-04803|TMC
+    &Duration=0&Units=1&Resolution=250&format=json
+
+    :see: http://docs.inrix.com/traffic/speed/#get-getsegmentspeed
+    """
+    url = "{}&segments={}".format(make_inrix_url("GetSegmentSpeed", force=force), segment_ids)
+    #print(url)
     return url
--- ott/trafficdb/control/inrix/speed_data.py
+++ ott/trafficdb/control/inrix/speed_data.py
@@ -1,11 +1,12 @@
 import time
-from .get_token import get_inrix_token
+import requests
+from .base import speeds_url_segments
 
 import logging
 log = logging.getLogger(__file__)
 
 
-def download_speed_data(count=2, num_tries=3):
+def download_speed_data(count=0, num_tries=3):
     """
     http://na.api.inrix.com/traffic/Inrix.ashx?format=json&action=getsecuritytoken&vendorid=<your vid>&consumerid=<your cid>
     will return json, ala { result: { token: < me> }, ... }
@@ -15,11 +16,15 @@ def download_speed_data(count=2, num_tries=3):
     ret_val = None
     try:
         log.info("get speed data")
-        token = get_inrix_token()
-        ret_val = token
-    except Exception as e:
-        if count <= num_tries:
-            time.sleep(5)
+        url = speeds_url_segments(force=(count > 0))
+        ret_val = requests.get(url).json()
+        if ret_val['statusText'] == 'TokenExpired':
+            if count < num_tries:
+                time.sleep(count * 5)
+                ret_val = download_speed_data(count + 1)
+    except:
+        if count < num_tries:
+            time.sleep(count * 5)
             ret_val = download_speed_data(count + 1)
         else:
             log.error("does ./config/base.ini have an 'inrix' section, with valid vendorid and consumerid values?")
